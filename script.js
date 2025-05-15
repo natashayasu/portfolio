f965f01887abba0004303070cda8a8e2dc7fd956
Natasha Kaori <natashayonamine@gmail.com>
2025-05-15 14:12:32 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,42 +1,5 @@
-const tema = document.querySelector("#toggle-theme");
+let tema = document.getElementById("btn-theme");
 
-tema.addEventListener("click", function () {
-  document.documentElement.classList.toggle("dark-mode");
-
-  if (document.documentElement.classList.contains("dark-mode")) {
-    tema.textContent = "Light mode";
-  } else {
-    tema.textContent = "Dark mode";
-  }
-});
-
-const menu = document.getElementById("menu");
-const menuBtn = document.getElementById("menu-btn");
-const closeBtn = document.getElementById("close-btn");
-
-function openMenu() {
-  menu.style.display = "flex";
-  menuBtn.style.display = "none";
-  closeBtn.style.display = "block";
+function mudarTema() {
+    document.body.classList.toggle("dark-mode");
 }
-
-function closeMenu() {
-  menu.style.display = "none";
-  menuBtn.style.display = "block";
-  closeBtn.style.display = "none";
-}
-
-function handleResize() {
-  if (window.innerWidth > 800) {
-    menu.style.display = "flex";
-    menuBtn.style.display = "none";
-    closeBtn.style.display = "none";
-  } else {
-    menu.style.display = "none";
-    menuBtn.style.display = "block";
-    closeBtn.style.display = "none";
-  }
-}
-
-window.addEventListener("load", handleResize);
-window.addEventListener("resize", handleResize);
