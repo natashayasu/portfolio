4c7d12a25bcc0a6acf351f0193160ee0f6e8bb45
Natasha Kaori <natashayonamine@gmail.com>
2025-06-10 11:30:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -30,3 +30,27 @@ function goBack() {
     document.getElementById('appFrame').src = "";
     document.getElementById('screen').classList.remove('show-app');
 }
+
+
+/* SCROLL */
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+
+    const id = link.getAttribute('href').substring(1);
+    const section = document.getElementById(id);
+    if (!section) return;
+
+    const sectionTop = section.getBoundingClientRect().top + window.pageYOffset;
+    const viewportHeight = window.innerHeight;
+    const sectionHeight = section.offsetHeight;
+    let scrollTo = sectionTop - (viewportHeight / 2) + (sectionHeight / 2);
+    if (scrollTo < 0) scrollTo = 0;
+
+    window.scrollTo({
+      top: scrollTo,
+      behavior: 'smooth'
+    });
+  });
+});
